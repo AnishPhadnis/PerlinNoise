ceecb33efa7940a3487bfd96df6078e4c4045344
AnishPhadnis <anishphadnis.ca@gmail.com>
2022-07-31 23:25:03 -0400

    slopes of each random value on graph

--- perlinNoise.py
+++ perlinNoise.py
@@ -1,34 +1,53 @@
+from cProfile import label
 import random
+from turtle import color
 import matplotlib.pyplot as plt
+
 
 #randomNums = [random.uniform(-1, 1) for i in range(10)]
 randomNums = [-0.8472978654485066, 0.9000528285750311, -0.4328894189757624, -0.0055113986838357665, -0.26868175295082874, -0.653747737881957, 0.6808501634140589, 0.6247107373599725, 0.6025352129131027, 0.4042941557771631, -0.18551517480776702]
 #print(randomNums)
 
+def getEquationOfLine(slope: int) -> callable:
+    def equation(y: int) -> int:
+        return slope * (    )
+
+randomNumsLine = map(lambda a: )
+
 interpolated = []
 incrementVal = 0.1
 for i in range(len(randomNums)-1):
-    print(f'Random num {i}')
-    distanceFromLeftPoint = 1.0
-    #print('-')
+    distanceFromLeftPoint = 0
+    
     for _ in range(int(1/incrementVal)):
-        #print(distanceFromLeftPoint)
-        newVal = randomNums[i] * distanceFromLeftPoint + randomNums[i+1] * (1-distanceFromLeftPoint)
+        newVal = randomNums[i] * (1-distanceFromLeftPoint) + randomNums[i+1]*distanceFromLeftPoint
+        print(i+distanceFromLeftPoint, newVal)
+        #plt.plot(i+distanceFromLeftPoint, newVal, 'ro')
+        
         interpolated.append(newVal)
-        distanceFromLeftPoint -= incrementVal
-    #print('-')
+        distanceFromLeftPoint += incrementVal
 
 interpolated.append(randomNums[-1])
 
-#print(len(interpolated))
 
-    
 
-plt.plot([i for i in range(len(randomNums))], randomNums)
+increment = 0.1
+for i in range(len(randomNums)):
+    #plt.axline((i, 0), slope=randomNums[i])
+    x = [i, i+increment, i-increment]
+    y = [0, increment/slope, -increment/slope]
+    #print(f'{i}: {x}\n{y}')
+    plt.plot(x, y)
+    plt.plot(i, 0, 'go')
 
-x = [i*incrementVal for i in range(len(randomNums) * int(1/incrementVal)+1)]
-print(x)
-plt.plot(x, interpolated)
+
+x = [i*incrementVal for i in range((len(randomNums)-1) * int(1/incrementVal)+1)]
+
+#plt.plot(x, interpolated, label='interpolated')
+
+plt.legend()
+plt.xlim(-1, 11)
+plt.ylim(-1, 1)
 plt.show()
 
 
