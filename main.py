8c9caa4abb4ac490d02d29b79ac4f4f91cf262d9
AnishPhadnis <anishphadnis.ca@gmail.com>
2022-08-02 23:57:18 -0400

    planned future parameters to implement

---
+++ main.py
@@ -0,0 +1,19 @@
+# how high and low values can go
+amplitude = 0 
+
+# how many perlin noise graphs to add for final result
+octaves = 0
+
+# increment of x value where random slope is generated?
+frequency = 0
+
+# how far zoomed in you are
+scale = 0
+
+# how much each successive octave affects final result
+persistence = 0
+
+# frequency of successive octaves
+lacunarity = 0
+
+
