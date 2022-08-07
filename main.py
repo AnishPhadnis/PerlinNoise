c2fd7b121dc11cf785d4d1347e57a1b1db4606d1
AnishPhadnis <anishphadnis.ca@gmail.com>
2022-08-06 22:55:07 -0400

    implemented working octave feature

--- main.py
+++ main.py
@@ -20,19 +20,56 @@ persistence = 0
 # frequency of successive octaves
 lacunarity = 0
 
-perlin = PerlinNoise(amplitude=10)
 
-x, y = [], []
+perlin1 = PerlinNoise(seed=1, amplitude=10, frequency=1)
+
+x, y1 = [], []
 for i in range(10):
     inc = i
+
     for _ in range(10):
         x.append(inc)
-        y.append(perlin.getNoiseAt(inc))
+        y1.append(perlin1.getNoiseAt(inc))
         
         inc += 0.1
+        
+        
+plt.plot(x, y1, 'r')
 
+perlin2 = PerlinNoise(seed=2, amplitude=5, frequency=0.5)
 
-plt.plot(x, y, 'y')
+x, y2 = [], []
+for i in range(10):
+    inc = i
+
+    for _ in range(10):
+        x.append(inc)
+        y2.append(perlin2.getNoiseAt(inc))
+        
+        inc += 0.1
+        
+#plt.plot(x, y2, 'g')
+
+perlin3 = PerlinNoise(seed=3, amplitude=2.5, frequency=0.25)
+
+x, y3 = [], []
+for i in range(10):
+    inc = i
+
+    for _ in range(10):
+        x.append(inc)
+        y3.append(perlin3.getNoiseAt(inc))
+        
+        inc += 0.1
+        
+plt.plot(x, y3, 'b')
+
+t = []
+for a, b, c in zip(y1, y2, y3):
+    t.append(a + c)
+
+plt.plot(x, t, 'y')
+
 plt.xlim(-1, 11)
 plt.ylim(-10, 10)
 plt.show()
--- noise.py
+++ noise.py
@@ -9,7 +9,7 @@ class PerlinNoise():
                  frequency: float = 1,
                  scale: float = 1,
                  octaves: float = 1,
-                 persistence: float = 1,
+                 persistence: float = 0.5,
                  lancunarity: float = 1
                  ):
         self.seed = seed # used
@@ -25,7 +25,6 @@ class PerlinNoise():
     # Extend for higher dimensions
     def getNoiseAt(self, x: float) -> float:
         if x % self.frequency == 0:
-            print(f'{x}')
             return 0
         
         lowerBound: int = int(x // self.frequency) * self.frequency
@@ -33,24 +32,33 @@ class PerlinNoise():
         
         distanceToLowerBound: float = x - lowerBound
         
-        lowerBoundSlope: float = self.getSlopeAt(lowerBound)
-        upperBoundSlope: float = self.getSlopeAt(upperBound)
-        
         smoothersteppedDiff: float = self.smootherstep(distanceToLowerBound)
         
-        yVal: float = self.getInterpolated(self.getY(lowerBoundSlope, lowerBound, x),
-                                           self.getY(upperBoundSlope, upperBound, x), smoothersteppedDiff
-                                           )
+        persistenceInc = 1
+        yVal = 0
+    
+        for octave in range(self.octaves):
+            lowerBoundSlope: float = self.getSlopeAt(lowerBound, octave, self.amplitude * persistenceInc)
+            upperBoundSlope: float = self.getSlopeAt(upperBound, octave, self.amplitude * persistenceInc)
         
-        print(f'{x}={lowerBound}-{upperBound} w/ {lowerBoundSlope}-{upperBoundSlope} -> {distanceToLowerBound}-{smoothersteppedDiff} -> {yVal}')
+        
+            result: float = self.getInterpolated(self.getY(lowerBoundSlope, lowerBound, x),
+                                            self.getY(upperBoundSlope, upperBound, x), smoothersteppedDiff
+                                            )
+
+            yVal += result 
+            persistenceInc *= self.persistence
+            
+            
+        
         return yVal
         
     # seed makes it so sequence of random num is always same
     # not that same x val returns same random num
     # no matter what x vals are, sequence of nums will always be same
     @functools.cache
-    def getSlopeAt(self, x: int, octave: int) -> float:
-        return random.uniform(-1 * self.amplitude, self.amplitude)
+    def getSlopeAt(self, x: int, octave: int, amplitude: float) -> float:
+        return random.uniform(-1 * amplitude, amplitude)
         
     
     def smootherstep(self, x: int) -> float:
